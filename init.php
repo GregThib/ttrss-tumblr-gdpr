0c0244eb0091743b180ab954a31d94d87b8387d4
Grégory T <legreg.thibault+github@gmail.com>
2018-05-23 16:12:17 +0200

    Changed buffer to 256 as in TT-RSS last version

--- init.php
+++ init.php
@@ -72,7 +72,7 @@ class Tumblr_GDPR extends Plugin {
 
 		// Download limit
 		curl_setopt($ch, CURLOPT_NOPROGRESS, false);
-		curl_setopt($ch, CURLOPT_BUFFERSIZE, 128); // needed to get 5 arguments in progress function?
+		curl_setopt($ch, CURLOPT_BUFFERSIZE, 256); // needed to get 5 arguments in progress function?
 		curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, function($curl_handle, $download_size, $downloaded, $upload_size, $uploaded) {
 			return ($downloaded > MAX_DOWNLOAD_FILE_SIZE) ? 1 : 0; // if max size is set, abort when exceeding it
 		});
